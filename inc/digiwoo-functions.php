6d987017b95787cdf9e8be14cf1aaf1149475f40
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-06 14:49:37 +0800

    Update digiwoo-functions.php

--- inc/digiwoo-functions.php
+++ inc/digiwoo-functions.php
@@ -109,6 +109,7 @@ add_filter('woocommerce_get_item_data', 'display_cart_item_add_ons', 10, 2);
 function clear_cart() {
     WC()->cart->empty_cart();
     wp_send_json_success();
+    WC()->cart->add_to_cart(19);
 }
 add_action('wp_ajax_clear_cart', 'clear_cart');
 add_action('wp_ajax_nopriv_clear_cart', 'clear_cart');
