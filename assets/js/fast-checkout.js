466e5aed8d4ce9578ed01427193af019413901a8
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-09 17:32:56 +0800

    update core

--- assets/js/fast-checkout.js
+++ assets/js/fast-checkout.js
@@ -29,6 +29,19 @@
 		console.log("Category ID:", categoryID);
 		console.log("Product ID:", productID);
 
+		$.ajax({
+	        type: 'POST',
+	        url: digiwoScriptVars.ajax_url,
+	        data: {
+	            'action': 'clear_and_add_to_cart',
+	            'product_id': productID,
+	        },
+	        success: function(response) {
+	            console.log(response);
+	        }
+	    });
+
+
 		$('input[name="product-category"][value="' + categoryID + '"]').prop('checked', true);
 		$('input[name="product-category"][value="' + categoryID + '"]').closest('.fast-checkout-radio-select-category').addClass('active');
 		$('.no-time-limit').show(); // Show the div
