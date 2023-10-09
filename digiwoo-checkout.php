91c0004333745050a2d74b9034ed62308f7d00bb
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-09 14:14:23 +0800

    Update digiwoo-checkout.php

--- digiwoo-checkout.php
+++ digiwoo-checkout.php
@@ -49,7 +49,7 @@ while ( have_posts() ) :
 			<!-- Cart Items -->
         <div class="fast-checkout-cart">
             <?php
-            // echo do_shortcode('[woocommerce_cart]');
+            	echo do_shortcode('[woocommerce_cart]');
             ?>
         </div>
 
