a26b4b38a03a33c8bf80e15236c4e09873ec1567
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-11 19:50:26 +0800

    update core

--- assets/js/fast-checkout.js
+++ assets/js/fast-checkout.js
@@ -93,21 +93,22 @@
 	        }).format(value);
 	    }
 
-	    function updateTotalOrder() {
-	        var productPrice = parseFloat($('input[name="product"]:checked').data('price') || 0);
-	        var addOnPrice = 0;
-	         $('input[name="add-on-trading[]"]:checked').each(function() {
-	            var addOnPercentage = parseFloat($(this).data('percentage') || 0);
-	            if (addOnPercentage) {
-	                addOnPrice += addOnPercentage * productPrice;
-	            } else {
-	                addOnPrice += parseFloat($(this).data('price') || 0);
-	            }
-	        });
-	        var total = productPrice + addOnPrice;
-	        $('#total-order-value').text(formatCurrency(total));
-	        $('.fast-checkout-total .woocommerce-Price-amount bdi').text(formatCurrency(total));
-	    }
+	    function updateTotalOrder(discountAmount = 0) {
+		    var productPrice = parseFloat($('input[name="product"]:checked').data('price') || 0);
+		    var addOnPrice = 0;
+		    $('input[name="add-on-trading[]"]:checked').each(function() {
+		        var addOnPercentage = parseFloat($(this).data('percentage') || 0);
+		        if (addOnPercentage) {
+		            addOnPrice += addOnPercentage * productPrice;
+		        } else {
+		            addOnPrice += parseFloat($(this).data('price') || 0);
+		        }
+		    });
+		    var total = productPrice + addOnPrice - discountAmount; // Subtract the discount
+		    $('#total-order-value').text(formatCurrency(total));
+		    $('.fast-checkout-total .woocommerce-Price-amount bdi').text(formatCurrency(total));
+		}
+
 
 	    $('button[name="apply_coupon"]').on('click', function(e) {
 	        e.preventDefault();
@@ -125,8 +126,14 @@
 	                if (response.success) {
 	                    jQuery(document.body).trigger('update_checkout');
                     	jQuery(document.body).trigger('wc_fragment_refresh');
-                    	jQuery('#total-order-value').text(formatCurrency(response.updated_total));
-                		jQuery('.fast-checkout-total .woocommerce-Price-amount bdi').text(formatCurrency(response.updated_total));
+
+                    	// If the response contains the discount amount
+				        if(response.data && response.data.discountAmount) {
+				            updateTotalOrder(response.data.discountAmount);
+				        } else {
+				            updateTotalOrder();
+				        }
+				        
 	                } else {
 	                    alert(response.data.message);
 	                }
