3b52348a17b62f236bf02d3c7de62b5a29a92379
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-13 17:12:52 +0800

    Update fast-checkout.js

--- assets/js/fast-checkout.js
+++ assets/js/fast-checkout.js
@@ -238,33 +238,6 @@
 
 
 	    $(document).on('change', 'input[name="product"]', function() {
-	    	var coupon_code = $('#displayed-coupon-code span[data-couponcode]').data('couponcode');
-		    console.log("Coupon code: ", coupon_code);  
-	        if ($('#displayed-coupon-code span[data-couponcode]').length) {
-		        // Extract the currently applied coupon code
-		        var coupon_code = $('#displayed-coupon-code span[data-couponcode]').data('couponcode');
-
-		        // Send an AJAX request to remove the coupon
-		        $.ajax({
-		            url: digiwoScriptVars.ajax_url,
-		            method: 'POST',
-		            data: {
-		                action: 'remove_coupon_code',
-		                coupon_code: coupon_code
-		            },
-		            success: function(response) {
-		                if (response.success) {
-		                    $('#displayed-coupon-code').empty();  // Remove displayed coupon code and the "Remove" button
-		                    jQuery(document.body).trigger('update_checkout');
-		                    jQuery(document.body).trigger('wc_fragment_refresh');
-		                } else {
-		                    alert(response.data.message);
-		                }
-		            }
-		        });
-			}
-			updateTotalOrder();
-									
 	    	var productId = $(this).val();
 	    	$('.spinner-order-total').show();
 	    	$('.checkout-order-total').hide(); 	
@@ -309,7 +282,6 @@
 						            });
 						            $('input[name="add-on-trading[]"]').prop('disabled', false);
 						        	$('.fast-checkout-radio-select-add-ons').removeClass('fast-checkout-btn-disable');
-
 						        } else {
 		                            alert('There was an error adding the product to the cart.');
 		                        }
@@ -327,6 +299,37 @@
 		    });
 
 	    });
+
+
+		$(document).on('change', 'input[name="product"]', function(e) {
+			e.stopPropagation();
+		    var coupon_code = $('#displayed-coupon-code span[data-couponcode]').data('couponcode');
+		    console.log("Coupon code: ", coupon_code);  
+	        if ($('#displayed-coupon-code span[data-couponcode]').length) {
+		        // Extract the currently applied coupon code
+		        var coupon_code = $('#displayed-coupon-code span[data-couponcode]').data('couponcode');
+
+		        // Send an AJAX request to remove the coupon
+		        $.ajax({
+		            url: digiwoScriptVars.ajax_url,
+		            method: 'POST',
+		            data: {
+		                action: 'remove_coupon_code',
+		                coupon_code: coupon_code
+		            },
+		            success: function(response) {
+		                if (response.success) {
+		                    $('#displayed-coupon-code').empty();  // Remove displayed coupon code and the "Remove" button
+		                    jQuery(document.body).trigger('update_checkout');
+		                    jQuery(document.body).trigger('wc_fragment_refresh');
+		                } else {
+		                    alert(response.data.message);
+		                }
+		            }
+		        });
+			}
+			updateTotalOrder();
+		});
 
 
 		var ajaxInProgress = false;
