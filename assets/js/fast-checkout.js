97edc0e6e64f986b2807c122f6ab2512ba13970a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-10-21 13:46:45 +0800

    Update fast-checkout.js

--- assets/js/fast-checkout.js
+++ assets/js/fast-checkout.js
@@ -93,7 +93,10 @@
 	        }).format(value);
 	    }
 
-	    function updateTotalOrder(discountAmount = 0) {
+		function updateTotalOrder() {
+		    // Fetch the subtotal from the WooCommerce cart summary
+		    var subtotal = parseFloat($('.woocommerce-Price-amount bdi').first().text().replace('$', ''));
+		    
 		    var productPrice = parseFloat($('input[name="product"]:checked').data('price') || 0);
 		    var addOnPrice = 0;
 		    $('input[name="add-on-trading[]"]:checked').each(function() {
@@ -104,10 +107,15 @@
 		            addOnPrice += parseFloat($(this).data('price') || 0);
 		        }
 		    });
-		    var total = productPrice + addOnPrice - discountAmount; // Subtract the discount
+		    
+		    var totalWithoutDiscount = productPrice + addOnPrice;
+		    var totalDiscount = totalWithoutDiscount - subtotal; // Calculate total discount
+		    var total = totalWithoutDiscount - totalDiscount; // Subtract the total discount
+		    
 		    $('#total-order-value').text(formatCurrency(total));
 		    $('.fast-checkout-total .woocommerce-Price-amount bdi').text(formatCurrency(total));
 		}
+
 
 	    $('button[name="apply_coupon"]').on('click', function(e) {
 	        e.preventDefault();
